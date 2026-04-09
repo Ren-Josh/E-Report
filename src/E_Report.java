54118ec165a930fcebb40d841b330de97041983c
Ren Josh <renjo.barrientos@gmail.com>
2026-04-09 18:38:37 +0800

    Added the main software class

---
+++ src/E_Report.java
@@ -0,0 +1,10 @@
+import services.controller.DatabaseController;
+
+public class E_Report {
+    public static void main(String[] args) {
+        // Initialize Database
+
+        DatabaseController.initializeDatabase();
+    }
+
+}
