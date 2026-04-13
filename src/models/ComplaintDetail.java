bbb0afe65f54c7d42e4572f3cd1cfdafe008b0ab
WatotBantot <amcapinpin23@gmail.com>
2026-04-13 10:26:25 +0800

    Repush

--- src/models/ComplaintDetail.java
+++ src/models/ComplaintDetail.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 public class ComplaintDetail {
     private String currentStatus, subject, type, street, purok, personsInvolved, details;
     private String photoAttachment;
+    private byte[] photoAttachmentBytes;
     private Timestamp dateTime;
     private double longitude, latitude;
 
@@ -114,6 +115,16 @@ public class ComplaintDetail {
 
     public void setPhotoAttachment(String photoAttachment) {
         this.photoAttachment = photoAttachment;
+        this.photoAttachmentBytes = null;
+    }
+
+    public byte[] getPhotoAttachmentBytes() {
+        return photoAttachmentBytes;
+    }
+
+    public void setPhotoAttachmentBytes(byte[] photoAttachmentBytes) {
+        this.photoAttachmentBytes = photoAttachmentBytes;
+        this.photoAttachment = null;
     }
 
 }
